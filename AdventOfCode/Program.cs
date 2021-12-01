2229d039063f2912c8334f09bf12decf73a9c116
Jan Karger ツ ☀ <punkerat76@gmail.com>
2021-12-01 10:59:48 +0100

    Initial commit

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,14 @@
+﻿if (args.Length == 0)
+{
+    Solver.SolveLast();
+}
+else if (args.Length == 1 && args[0].Contains("all", StringComparison.CurrentCultureIgnoreCase))
+{
+    Solver.SolveAll();
+}
+else
+{
+    var indexes = args.Select(arg => uint.TryParse(arg, out var index) ? index : uint.MaxValue);
+
+    Solver.Solve(indexes.Where(i => i < uint.MaxValue));
+}
