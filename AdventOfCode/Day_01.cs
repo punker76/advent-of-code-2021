e7de810d9f44eadd2d59c212cb51fb289740a1af
punker76 <punkerat76@gmail.com>
2021-12-01 12:33:45 +0100

    Day 1, part 2

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -11,21 +11,40 @@ public class Day_01 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        // int? prevMeasurement = null;
+        var increasedValues = GetIncreasedValues(_input.Select(x => Convert.ToInt32(x)).ToArray());
+
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 is {increasedValues}");
+    }
+
+    private int GetIncreasedValues(int[] input)
+    {
         int increasedValues = 0;
 
-        for (int i = 1; i < _input.Length; i++)
+        for (int i = 1; i < input.Length; i++)
         {
-            var prevDepth = Convert.ToInt32(_input[i - 1]);
-            var depth = Convert.ToInt32(_input[i]);
+            var prevDepth = input[i - 1];
+            var depth = input[i];
             if (depth > prevDepth)
             {
                 increasedValues++;
             }
         }
 
-        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 is {increasedValues}");
+        return increasedValues;
     }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2()
+    {
+        var threeSums = new List<int>();
+
+        for (int i = 0; i < _input.Length - 2; i++)
+        {
+            var threeSum = _input.Take(new Range(i, i + 3)).Select(x => Convert.ToInt32(x)).Sum();
+            threeSums.Add(threeSum);
+        }
+
+        var increasedValues = GetIncreasedValues(threeSums.ToArray());
+
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 is {increasedValues}");
+    }
 }
