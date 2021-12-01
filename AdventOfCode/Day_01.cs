e93462f6cf342212cbbd9df27cfe90c39a9e7b69
punker76 <punkerat76@gmail.com>
2021-12-01 11:47:44 +0100

    Day 1, part 1

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -2,14 +2,30 @@
 
 public class Day_01 : BaseDay
 {
-    private readonly string _input;
+    private readonly string[] _input;
 
     public Day_01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllText(InputFilePath).Split('\n', StringSplitOptions.RemoveEmptyEntries);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        // int? prevMeasurement = null;
+        int increasedValues = 0;
+
+        for (int i = 1; i < _input.Length; i++)
+        {
+            var prevDepth = Convert.ToInt32(_input[i - 1]);
+            var depth = Convert.ToInt32(_input[i]);
+            if (depth > prevDepth)
+            {
+                increasedValues++;
+            }
+        }
+
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 is {increasedValues}");
+    }
 
     public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
 }
