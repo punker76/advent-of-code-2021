92cef00da63d40009654f9a81d0561845b424738
punker76 <punkerat76@gmail.com>
2021-12-03 11:35:34 +0100

    Day 3, part 2

--- AdventOfCode/Day_03.cs
+++ AdventOfCode/Day_03.cs
@@ -38,7 +38,40 @@ public class Day_03 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        var result = 0;
+        var oxygen = _input.Select(x => x.ToCharArray()).ToArray();
+        var co2 = _input.Select(x => x.ToCharArray()).ToArray();
+
+        var colLength = oxygen[0].Length;
+
+        for (int col = 0; col < colLength; col++)
+        {
+            var zeroCount = oxygen.Count(x => x[col] == '0');
+            var nonZeroCount = oxygen.Count(x => x[col] == '1');
+            if (zeroCount < nonZeroCount || zeroCount == nonZeroCount)
+            {
+                oxygen = oxygen.Where(x => x[col] == '1').ToArray();
+            }
+            else
+            {
+                oxygen = oxygen.Where(x => x[col] == '0').ToArray();
+            }
+
+            if (co2.Length > 1)
+            {
+                zeroCount = co2.Count(x => x[col] == '0');
+                nonZeroCount = co2.Count(x => x[col] == '1');
+                if (zeroCount < nonZeroCount || zeroCount == nonZeroCount)
+                {
+                    co2 = co2.Where(x => x[col] == '0').ToArray();
+                }
+                else
+                {
+                    co2 = co2.Where(x => x[col] == '1').ToArray();
+                }
+            }
+        }
+
+        var result = Convert.ToInt32(new string(oxygen[0]), 2) * Convert.ToInt32(new string(co2[0]), 2);
 
         return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 is {result}");
     }
