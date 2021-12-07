69e61a0d054b0bc172cafcd222bd1210e73659a6
punker76 <punkerat76@gmail.com>
2021-12-07 09:00:39 +0100

    Day 7, part 1

---
+++ AdventOfCode/Day_07.cs
@@ -0,0 +1,57 @@
+﻿namespace AdventOfCode;
+
+public class Day_07 : BaseDay
+{
+    private class Crab
+    {
+        public int position;
+
+        public Crab(int position)
+        {
+            this.position = position;
+        }
+
+        public int GetFuelForPosition(int toPos)
+        {
+            return Math.Abs(position - toPos);
+        }
+    }
+
+    private readonly string _input;
+    private readonly List<Crab> _crabs;
+
+    public Day_07()
+    {
+        // _input = "16,1,2,0,4,2,7,1,2,14";
+        _input = File.ReadAllLines(InputFilePath) [0];
+        _crabs = _input
+            .Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .Select(p => new Crab(int.Parse(p)))
+            .ToList();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var maxPosition = _crabs.Max(c => c.position);
+        var lowestFuel = (pos: 0, fuel: int.MaxValue);
+
+        for (int pos = 0; pos <= maxPosition; pos++)
+        {
+            var fuel = _crabs.Sum(c => c.GetFuelForPosition(pos));
+            if (fuel < lowestFuel.fuel)
+            {
+                lowestFuel.fuel = fuel;
+                lowestFuel.pos = pos;
+            }
+        }
+
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 is {lowestFuel.fuel} on {lowestFuel.pos}");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var result = 0;
+
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 is {result}");
+    }
+}
